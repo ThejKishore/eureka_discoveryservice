7a7b5710aac7da40bf81618f82e5c51c7fade151
Thej Kishore <kishores1984@gmail.com>
2017-10-27 00:27:48 -0500

    initial commit in eureka server sample

---
+++ readme.md
@@ -0,0 +1,3 @@
+[pcf eureka-reference document](https://docs.pivotal.io/spring-cloud-services/1-4/common/service-registry/oss.html)
+
+https://projects.spring.io/spring-cloud/
---
+++ src/main/java/com/kish/euerkaserver/EuerkaserverApplication.java
@@ -0,0 +1,14 @@
+package com.kish.euerkaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EuerkaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuerkaserverApplication.class, args);
+	}
+}
